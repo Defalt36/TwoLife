a01009ddcf7afc31392fec601581809386cf26fb
Adk9p <Adk9p@users.noreply.github.com>
2020-01-06 09:14:14 -0800

    Added skip on nude parts

--- gameSource/animationBank.cpp
+++ gameSource/animationBank.cpp
@@ -8,6 +8,7 @@
 #include "math.h"
 
 #include "minorGems/util/SimpleVector.h"
+#include "minorGems/util/SettingsManager.h"
 #include "minorGems/io/file/File.h"
 
 #include "minorGems/game/gameGraphics.h"
@@ -30,6 +31,7 @@ static int mapSize;
 // sparse, so some entries are NULL
 static AnimationRecord * **idMap;
 
+int NudeToggle;
 
 // maps IDs to arbitrary number of extra animation records 
 // non-sparse
@@ -163,6 +165,8 @@ int initAnimationBankStart( char *outRebuildingCache ) {
         }
     
 
+    NudeToggle =
+        SettingsManager::getIntSetting( "nudeEnabled", 1 );
 
     maxID = 0;
 
@@ -2024,6 +2028,9 @@ HoldingPos drawObjectAnim( int inObjectID, int inDrawBehindSlots,
     SimpleVector <int> legIndices;
     getAllLegIndices( obj, inAge, &legIndices );
 
+    SimpleVector <int> nudeIndices;
+    getAllNudeIndices( obj, inAge, &nudeIndices );
+
 
     // worn clothing never goes to ground animation
     // switches between moving, when the wearer is moving, and held,
@@ -2687,6 +2694,13 @@ HoldingPos drawObjectAnim( int inObjectID, int inDrawBehindSlots,
 
         char skipSprite = false;
         
+        if( obj->person ) {
+            if( nudeIndices.getElementIndex(i) != -1 ) {
+                if( !NudeToggle ) {
+                    skipSprite = true;
+                }
+            }
+        }
 
         if( headless &&
             headlessSkipFlags[i] ) {

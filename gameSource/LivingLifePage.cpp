5fe50526d19826c4f4890ed0a2002941875c3a7b
risvh <67486979+risvh@users.noreply.github.com>
2021-04-03 23:21:02 +0800

    use server age for speech limit

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -22161,7 +22161,7 @@ void LivingLifePage::step() {
         
 
         // current age
-        double age = computeCurrentAge( ourLiveObject );
+        double age = computeCurrentAgeNoOverride( ourLiveObject );
 
         int sayCap = getSayLimit( age );
 

97b3845aaf25bfe8f72771afecfbeaf7142bf109
risvh <67486979+risvh@users.noreply.github.com>
2021-09-19 00:54:23 +0800

    new move for object-object interactions
    Object A will move on to another object B if they have a transition A + B = C + D.

--- gameSource/transitionBank.cpp
+++ gameSource/transitionBank.cpp
@@ -3167,6 +3167,9 @@ void printTrans( TransRecord *inTrans ) {
             case 7:
                 moveName = "wst";
                 break;
+            case 8:
+                moveName = "find";
+                break;
             }
         
 
